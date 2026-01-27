546bf4acec8f94951e2c356f935d639108b89c33
debasish <debasishparhi544@gmail.com>
2026-01-27 11:28:15 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<h1>Product list component</h1>}></Route>
+          <Route path='/add' element={<h1>Add list component</h1>}></Route>
+          <Route path='/update' element={<h1>Update list component</h1>}></Route>
+          <Route path='/logout' element={<h1>Logout component</h1>}></Route>
+          <Route path='/profile' element={<h1>Profile component</h1>}></Route>
+        </Routes>
+      </BrowserRouter>
+
+      <Footer/>
+
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <h3>e-com dashboard footer</h3>
+    </div>
+  )
+}
+
+export default Footer
